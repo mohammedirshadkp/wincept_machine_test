4e7a2ac80dc9863a7a16ddec89d9088cf0fea46e
irshad500 <mohammedirshadkp500@gmail.com>
2024-01-05 12:07:08 +0530

    Initial commit

---
+++ lib/login.dart
@@ -0,0 +1,109 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:wincept/thread.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              controller: _usernameController,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: () {
+                String username = _usernameController.text;
+                String password = _passwordController.text;
+                login(username, password, context);
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+login(String username, String password, BuildContext context) async {
+  var headers = {
+    'Content-Type': 'application/json',
+    'User-Agent': 'insomnia/8.5.0',
+    'Authorization': 'Token 44f91df7464f18117c3b52d77cb054bbdf34ee0b'
+  };
+
+  var url = 'https://stuverse.shop/api/login/';
+  var body = json.encode({"username": username, "password": password});
+
+  try {
+    var response = await http.post(
+      Uri.parse(url),
+      headers: headers,
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      var jsonResponse = json.decode(response.body);
+      print(jsonResponse);
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ThreadPage(),
+          ));
+    } else {
+      print('Login failed with status code: ${response.statusCode}');
+      print('Login failed with status code: ${response.statusCode}');
+      print('Login failed with status code: ${response.statusCode}');
+      print('Login failed with status code: ${response.statusCode}');
+      print('Login failed with status code: ${response.statusCode}');
+      print('Login failed with status code: ${response.statusCode}');
+      print('Login failed with status code: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error during login: $e');
+    print('Error during login: $e');
+    print('Error during login: $e');
+    print('Error during login: $e');
+    print('Error during login: $e');
+    print('Error during login: $e');
+    print('Error during login: $e');
+    print('Error during login: $e');
+  }
+}
---
+++ lib/thread.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ThreadPage extends StatefulWidget {
+  const ThreadPage({super.key});
+
+  @override
+  _ThreadPageState createState() => _ThreadPageState();
+}
+
+class _ThreadPageState extends State<ThreadPage> {
+  List<String> threads = [];
+
+  void _addThread(String thread) {
+    setState(() {
+      threads.add(thread);
+    });
+  }
+
+  Future<void> _createThread(String thread) async {
+    var headers = {
+      'Content-Type': 'multipart/form-data',
+      'User-Agent': 'insomnia/8.5.0',
+      'Authorization': 'Token 44f91df7464f18117c3b52d77cb054bbdf34ee0b'
+    };
+
+    var request = http.MultipartRequest(
+        'POST', Uri.parse('https://stuverse.shop/api/threads/'));
+
+    request.headers.addAll(headers);
+    request.fields.addAll({
+      'content': thread,
+    });
+    try {
+      http.StreamedResponse response = await request.send();
+
+      if (response.statusCode == 201) {
+        print('Thread created successfully!');
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Thread created successfully!'),
+          ),
+        );
+      } else {
+        print('Failed to create thread: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      print('Error creating thread: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Thread App'),
+      ),
+      body: ListView.builder(
+        itemCount: threads.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(threads[index]),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var result = await _navigateToAddThreadScreen(context);
+          if (result != null) {
+            _createThread(result);
+            _addThread(result);
+          }
+        },
+        tooltip: 'Add Thread',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Future<String?> _navigateToAddThreadScreen(BuildContext context) async {
+    return await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => AddThreadScreen()),
+    );
+  }
+}
+
+class AddThreadScreen extends StatelessWidget {
+  final TextEditingController _threadController = TextEditingController();
+
+  AddThreadScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Thread'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              controller: _threadController,
+              decoration: const InputDecoration(
+                labelText: 'Enter Thread',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                String thread = _threadController.text;
+                Navigator.pop(context, thread);
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
